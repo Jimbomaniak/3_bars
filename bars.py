5ebd4226494282110f6ef9ae0da44ded870d6f10
Olegka <jimboman54321@gmail.com>
2016-09-27 17:32:58 +0300

    готово

--- bars.py
+++ bars.py
@@ -2,20 +2,39 @@ import json
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, encoding='utf-8') as file:
+        data = json.load(file)
+    return data 
 
 
 def get_biggest_bar(data):
-    pass
+    res = {data[x]['Cells']['Name']:data[x]['Cells']['SeatsCount'] for x in range(0,len(data))}
+    name = max(res, key=res.get)
+    return name
 
 
 def get_smallest_bar(data):
-    pass
+    res = {data[x]['Cells']['Name']:data[x]['Cells']['SeatsCount'] for x in range(0,len(data))}
+    name = min(res, key=res.get)
+    return name
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+    res = {data[x]['Cells']['Name']:
+sum([abs(
+        abs(data[x]['Cells']['geoData']['coordinates'][0]) - abs(longitude)
+        ),
+    abs(
+        abs(data[x]['Cells']['geoData']['coordinates'][1]) - abs(latitude)
+        )
+    ]) for x in range(0,len(data))}
+    name = min(res, key=res.get)
+    return name
 
 
 if __name__ == '__main__':
-    pass
+    print(get_biggest_bar(load_data('бары.json')))
+    print(get_smallest_bar(load_data('бары.json')))
+    longitude = float(input('Введите долготу, пример 24.12444'))
+    latitude = float(input('Введите широту, пример 24.12444'))
+    print(get_closest_bar(load_data('бары.json'), longitude, latitude))
