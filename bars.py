d5780a7abb71ab3dc3e0803ddf40325d01f21f62
Olegka <jimboman54321@gmail.com>
2016-10-01 22:36:18 +0300

    try to create pure functions,fix distance calculation

--- bars.py
+++ bars.py
@@ -1,34 +1,35 @@
 import json
+from geopy.distance import great_circle
 
 
-def load_data():
-    filepath = input('Write your path to directory with json file(like c:/users/bars.json): ')
-    try:
-        with open(filepath, encoding='utf-8') as file:
-            data = json.load(file)
-        return data 
-    except FileNotFoundError:
-        print('No such file in directory')
-        return load_data()
+def load_data(filepath):
+    with open(filepath, encoding='utf-8') as file:
+        data = json.load(file)
+    return data
 
 
 def get_biggest_bar(data):
-    res = {data['Cells']['Name']:data['Cells']['SeatsCount'] for data in data}
-    biggest_bar = max(res, key=res.get)
+    barnames_seatscount = {bar['Cells']['Name']:bar['Cells']['SeatsCount'] for bar in data}
+    biggest_bar = max(barnames_seatscount, key=barnames_seatscount.get)
     return biggest_bar
 
 
 def get_smallest_bar(data):
-    res = {data['Cells']['Name']:data['Cells']['SeatsCount'] for data in data}
-    smallest_bar = min(res, key=res.get)
+    barnames_seatscount = {bar['Cells']['Name']:bar['Cells']['SeatsCount'] for bar in data}
+    smallest_bar = min(barnames_seatscount, key=barnames_seatscount.get)
     return smallest_bar
 
 
-def get_closest_bar(data):
-    user_coordinates = get_coordinates()
-    all_distances_to_bars = {bar['Cells']['Name']:get_distance_to_bar(bar['Cells']['geoData']['coordinates'],user_coordinates) for bar in data}
-    closest_bar = min(all_distances_to_bars, key=all_distances_to_bars.get)
+def get_closest_bar(data, user_coordinates):
+    all_bar_distances = {}
+    for bar in data:
+        bar_coordinates = bar['Cells']['geoData']['coordinates']
+        bar_name = bar['Cells']['Name']
+        distance = great_circle(user_coordinates, bar_coordinates).kilometers
+        all_bar_distances[bar_name] = distance
+    closest_bar = min(all_bar_distances, key=all_bar_distances.get)
     return closest_bar
+
 
 def get_coordinates():
     try:
@@ -37,20 +38,21 @@ def get_coordinates():
         user_coordinates = [longitude, latitude]
         return user_coordinates
     except ValueError:
-        print('Write only integers')
+        print('Enter digits only')
         return get_coordinates()
-        
+
+
 def get_distance_to_bar(bar_coordinates, user_coordinates):
     user_long, user_lat = user_coordinates
-    bar_long , bar_lat = bar_coordinates
-    distance_to_bar = sum([abs(bar_long - user_long),abs(bar_lat - user_lat)])
+    bar_long, bar_lat = bar_coordinates
+    distance_to_bar = sum([abs(bar_long - user_long), abs(bar_lat - user_lat)])
     return distance_to_bar
 
 
-
-
 if __name__ == '__main__':
-    data = load_data()
+    filepath = input('Path to json file(e.g. /c:/users/bars.json): ')
+    data = load_data(filepath)
     print('The biggest bar is: {0}'.format(get_biggest_bar(data)))
     print('The smallest bar is: {0}'.format(get_smallest_bar(data)))
-    print('The closest bar to you is: {0}'.format(get_closest_bar(data)))
+    user_coordinates = get_coordinates()
+    print('The closest bar is: {0}'.format(get_closest_bar(data, user_coordinates)))
